db393c9f6f20316ad12adf3dd769fa9936e0b0c1
luizHenriq12 <luiz.henriq1206@gmail.com>
2023-10-05 21:30:44 -0300

    Tarefas

---
+++ quadro-de-tarefa/src/App.tsx
@@ -0,0 +1,17 @@
+import { Header } from "./components/Header"
+import { GlobalStyle } from "./styles/global"
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <GlobalStyle />
+    <Header />
+      <h1>Hello Word</h1>
+    </>
+  )
+}
+
+export default App
